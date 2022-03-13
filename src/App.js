8d36abac49cd0c1dbf0dd7267089506a986bc368
ziyuan-h <59698462+ziyuan-h@users.noreply.github.com>
2022-03-13 18:27:51 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,8 +15,6 @@ const decodeFileBase64 = (base64String) => {
 function App() {
   const apiUrl = 'https://l5c29682bl.execute-api.us-east-1.amazonaws.com/dev/';
 
-  // const [inputFileData, setInputFileData] = React.useState(''); // represented as bytes data (string)
-  // const [outputFileData, setOutputFileData] = React.useState(''); // represented as readable data (text string)
   const [buttonDisable, setButtonDisable] = React.useState(false);
   const [buttonText, setButtonText] = React.useState('Submit');
 
@@ -26,100 +24,10 @@ function App() {
   const [company, setCompany] = React.useState('goog');
 
   // output console
-  const [outputConsole, setOutputConsole] = React.useState('');
+  const [outputConsole, setOutputConsole] = React.useState("output");
 
   // debug use
   const [textBox, setTextBox] = React.useState("debug");
-
-  // // convert file to bytes data
-  // const convertFileToBytes = (inputFile) => {
-  //   console.log('converting file to bytes...');
-  //   return new Promise((resolve, reject) => {
-  //     const fileReader = new FileReader();
-  //     fileReader.readAsDataURL(inputFile); // reads file as bytes data
-
-  //     fileReader.onload = () => {
-  //       resolve(fileReader.result);
-  //     };
-
-  //     fileReader.onerror = (error) => {
-  //       reject(error);
-  //     };
-  //   });
-  // }
-
-  // handle file input
-  // const handleChange = async (event) => {
-  //   // Clear output text.
-  //   setOutputFileData("");
-
-  //   console.log('newly uploaded file');
-  //   const inputFile = event.target.files[0];
-  //   console.log(inputFile);
-
-  //   // convert file to bytes data
-  //   const base64Data = await convertFileToBytes(inputFile);
-  //   const base64DataArray = base64Data.split('base64,'); // need to get rid of 'data:image/png;base64,' at the beginning of encoded string
-  //   const encodedString = base64DataArray[1];
-  //   setInputFileData(encodedString);
-  //   console.log('file converted successfully');
-
-  //   // enable submit button
-  //   setButtonDisable(false);
-  // }
-
-  // // handle file submission
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-
-  //   // temporarily disable submit button
-  //   setButtonDisable(true);
-  //   setButtonText('Loading Result');
-
-  //   // make POST request
-  //   console.log('making POST request...');
-  //   fetch('<api-url>', {
-  //     method: 'POST',
-  //     headers: { "Content-Type": "application/json", "Accept": "text/plain" },
-  //     body: JSON.stringify({ "image": inputFileData })
-  //   }).then(response => response.json())
-  //   .then(data => {
-  //     console.log('getting response...');
-  //     console.log(data);
-
-  //     // POST request error
-  //     if (data.statusCode === 400) {
-  //       const outputErrorMessage = JSON.parse(data.errorMessage)['outputResultsData'];
-  //       setOutputFileData(outputErrorMessage);
-  //     }
-
-  //     // POST request success
-  //     else {
-  //       setOutputConsole("Input submitted successfully.\n Waiting for training results...\n")
-  //       // continuous GET requests until success
-  //       while (true) {
-  //         fetch(apiUrl).then(response => response.json())
-  //           .then(data => {
-  //             if (data.statusCode == 400) {
-
-  //             } else {
-  //               var imageBytesData = JSON.parse(data.body)["result_img"]
-  //             }
-  //           })
-  //       }
-
-  //       // const outputBytesData = JSON.parse(data.body)['outputResultsData'];
-  //       // setOutputFileData(decodeFileBase64(outputBytesData));
-  //     }
-
-  //     // re-enable submit button
-  //     setButtonDisable(false);
-  //     setButtonText('Submit');
-  //   })
-  //   .then(() => {
-  //     console.log('POST request success');
-  //   })
-  // }
 
   // handle initial money input
   const inputInitialMoney = async (event) => {
@@ -136,7 +44,7 @@ function App() {
     setCompany(list.options[list.selectedIndex].value);
   }
 
-  const handleSubmitDebug = async (event) => {
+  const handleSubmitDebug = (event) => {
     event.preventDefault();
 
     // update debug message
@@ -155,22 +63,25 @@ function App() {
       body: JSON.stringify({ "model_params": timeRange+','+company+','+initialMoney })
     }).then(response => response.json())
     .then(data => {
-      console.log('getting response...')
+      console.log('getting response...');
       console.log(data);
 
       // POST request error
       if (data.statusCode === 400) {
         const outputErrorMessage = JSON.parse(data.errorMessage)['outputResultsData'];
         setOutputConsole(outputErrorMessage);
+        console.log("fail to submit POST request")
       }
 
       // POST request success
       else {
+        console.log("successfully submitted POST request, trying to GET result...")
         setOutputConsole("Input submitted successfully.\n Waiting for training results......")
         // start submitting GET requests and wait for the results to be downloaded
         var resultReceived = false;
         while (!resultReceived) {
-          fetch(apiUrl).then(response => response.json())
+          setTimeout(() => {
+            fetch(apiUrl).then(response => response.json())
             .then(data => {
               // the get requset failed
               if (data.statusCode == 400) {
@@ -178,8 +89,6 @@ function App() {
               } else {
                 // parse the result image
                 var imageBytesData = JSON.parse(data.body)["result_img"]
-
-
                 // parse the result type
                 var textData = JSON.parse(data.body)["result_txt"]
                 setTextBox(textData["buy"])
@@ -188,6 +97,7 @@ function App() {
                 resultReceived = true;
               }
             })
+          }, 15000); // execute get request every 15 seconds
         } // end while
       }
 
