7196411c3768117059f4bbf2e478db931b78dc19
ziyuan-h <ziyuanh@umich.edu>
2022-03-13 23:53:02 -0400

    image

--- src/App.js
+++ src/App.js
@@ -208,7 +208,7 @@ function App() {
         </p>
       </div>
       <div>
-        <img id="Image" src={outputImg} alt="result figure" hidden={} />
+        <img id="Image" src={outputImg} alt="result figure" />
       </div>
     </div>
   );
